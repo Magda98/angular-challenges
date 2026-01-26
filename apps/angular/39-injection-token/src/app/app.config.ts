9653ffb172ffa4766758606fdb55663c5a521964
Magdalena Bober <magdakochman7@gmail.com>
2026-01-26 20:23:26 +0100

    feat(39-injection-token): use injection token

--- apps/angular/39-injection-token/src/app/app.config.ts
+++ apps/angular/39-injection-token/src/app/app.config.ts
@@ -1,12 +1,21 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { TimerToken } from './data';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter([
       { path: '', pathMatch: 'full', redirectTo: 'video' },
-      { path: 'video', loadComponent: () => import('./video.component') },
-      { path: 'phone', loadComponent: () => import('./phone.component') },
+      {
+        path: 'video',
+        loadComponent: () => import('./video.component'),
+        providers: [{ provide: TimerToken, useValue: 1000 }],
+      },
+      {
+        path: 'phone',
+        loadComponent: () => import('./phone.component'),
+        providers: [{ provide: TimerToken, useValue: 2000 }],
+      },
     ]),
   ],
 };
--- apps/angular/39-injection-token/src/app/data.ts
+++ apps/angular/39-injection-token/src/app/data.ts
@@ -1,1 +1,5 @@
+import { InjectionToken } from '@angular/core';
+
 export const DEFAULT_TIMER = 1000;
+
+export const TimerToken = new InjectionToken<number>('TimerToken');
